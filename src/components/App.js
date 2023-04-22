62e0c2300023b66fe028b25c8c0b98be98dc8dbf
Kiru <kelvin.kipruto@student.moringaschool.com>
2023-04-23 01:15:21 +0300

    🔥used arrow funtion instead

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ColorList from "./ColorList";
 
-function App() {
+export const App= ()=> {
   return (
     <div>
       <ColorList />
@@ -9,4 +9,4 @@ function App() {
   );
 }
 
-export default App;
+
